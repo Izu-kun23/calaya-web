5492afa9324cb4b9fd7cdb4450a676cac66f1ac2
Izuchukwu Tony <izuchukwuonuoha6@gmail.com>
2025-09-06 14:05:12 +0100

    Initial commit: Calaya Engineering website

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/calaya-web/', // Replace 'calaya-web' with your actual repository name
 })
